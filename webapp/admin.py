e60e658500ee7a66250ee756e3aede96d41798cd
MP <pavlicic1marko@gmail.com>
2026-08-11 18:39:16 +0200

    add Mettings modal, register it

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Metting
 
 
 @admin.register(Message)
@@ -8,3 +8,11 @@ class MessageAdmin(admin.ModelAdmin):
 	search_fields = ("name", "email", "subject", "message")
 	list_filter = ("created_at",)
 	ordering = ("-created_at",)
+
+
+@admin.register(Metting)
+class MettingAdmin(admin.ModelAdmin):
+	list_display = ("name", "email", "phone_number", "date", "timeslot", "crated_at")
+	search_fields = ("name", "email", "phone_number")
+	list_filter = ("date", "timeslot", "crated_at")
+	ordering = ("-crated_at",)
--- webapp/models.py
+++ webapp/models.py
@@ -11,3 +11,21 @@ class Message(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class Metting(models.Model):
+    class TimeSlot(models.TextChoices):
+        SLOT_1500 = "15:00", "15:00"
+        SLOT_1530 = "15:30", "15:30"
+        SLOT_1600 = "16:00", "16:00"
+        SLOT_1630 = "16:30", "16:30"
+
+    date = models.DateField()
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    timeslot = models.CharField(max_length=5, choices=TimeSlot.choices)
+    crated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} {self.timeslot}"
