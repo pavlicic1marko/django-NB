ed5317f23635f06584b54d7d5a0bd8ae567e82ce
MP <pavlicic1marko@gmail.com>
2026-08-31 12:47:20 +0200

    fix news plural

--- webapp/models.py
+++ webapp/models.py
@@ -61,14 +61,17 @@ class Message(models.Model):
 
 
 class News(models.Model):
-    verbose_name = "News article"
-    verbose_name_plural = "News"
+
     title = models.CharField(max_length=200, unique=True)
     text = models.TextField()
     date = models.DateField()
     # TODO: Add an alt_text field when image metadata is managed in the database.
     image = models.ImageField(upload_to="news/")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "News article"
+        verbose_name_plural = "News"
 
     def __str__(self):
         return self.title
