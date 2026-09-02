4780d6f802d7713c962550844128fd8da5d944ba
MP <pavlicic1marko@gmail.com>
2026-09-02 20:45:56 +0200

    redirect user to home after login

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
