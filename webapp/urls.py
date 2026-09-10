f89b9651308efd06c1a71079afb01ba32205d1df
MP <pavlicic1marko@gmail.com>
2026-09-10 16:45:41 +0200

    merge main into current

--- webapp/urls.py
+++ webapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path(_("chat/"), views.chat, name="chat"),
     path(_("login/"), auth_views.LoginView.as_view(template_name="website/login.html"), name="login"),
     path(_("logout/"), auth_views.LogoutView.as_view(), name="logout"),
+    path(_("ai-lab/"), views.ai_lab, name="ai_lab"),
     path(_("contact/"), views.contact, name="contact"),
     path(_("schedule-meeting/"), views.contact, name="schedule_meeting"),
     path(_("blog/"), views.blog, name="blog"),
