7db0df377c89653ae1a9d6dd09e3a7e9279bd8de
MP <pavlicic1marko@gmail.com>
2026-09-25 10:01:23 +0200

    adding tests to check text on page title, h1 elements

--- tests/playwright/test_first.py
+++ tests/playwright/test_first.py
@@ -2,4 +2,10 @@ def test_home_page(page):
     page.goto("http://127.0.0.1:8000/")
 
     assert page.title() != ""
-    assert page.locator("body").is_visible()
+    assert page.locator("h1", has_text="We build AI products and integrations that move your business faster.").is_visible()
+
+def test_about_page(page):
+    page.goto("http://127.0.0.1:8000/about-us/")
+
+    assert page.title() != ""
+    assert page.locator("h1", has_text="Who We Are").is_visible()
