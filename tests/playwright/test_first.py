e6ac616f1136bbb43281447dc64b20844cfd625e
MP <pavlicic1marko@gmail.com>
2026-09-25 16:12:30 +0200

    adding regression test

--- tests/playwright/test_first.py
+++ tests/playwright/test_first.py
@@ -3,6 +3,7 @@ from pages.home_page import HomePage
 from pages.about_page import AboutPage
 
 @pytest.mark.smoke
+@pytest.mark.regression
 def test_home_page(page):
     home = HomePage(page)
     home.load()
@@ -11,9 +12,18 @@ def test_home_page(page):
     assert home.is_heading_visible()
 
 @pytest.mark.smoke
+@pytest.mark.regression
 def test_about_page(page):
     about = AboutPage(page)
     about.load()
 
     assert about.get_title() != ""
     assert about.is_heading_visible()
+
+@pytest.mark.regression
+def test_home_page_heading_is_not_wrong_text(page):
+    home = HomePage(page)
+    home.load()
+
+    actual_text = page.locator("h1").first.text_content()
+    assert actual_text != "This is definitely the wrong heading"
