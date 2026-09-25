a92f1419c7e449edf0d9c950245cd6e91ef00aba
MP <pavlicic1marko@gmail.com>
2026-09-25 10:17:33 +0200

    adding anotations for smoke and regression tests

--- tests/playwright/test_first.py
+++ tests/playwright/test_first.py
@@ -1,9 +1,13 @@
+import pytest
+
+@pytest.mark.smoke
 def test_home_page(page):
     page.goto("http://127.0.0.1:8000/")
 
     assert page.title() != ""
     assert page.locator("h1", has_text="We build AI products and integrations that move your business faster.").is_visible()
 
+@pytest.mark.smoke
 def test_about_page(page):
     page.goto("http://127.0.0.1:8000/about-us/")
 
